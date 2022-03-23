be5e5bb9ebc22d1471eb555f945e6452f0aa42ae
samandar-dev <samandarbohodirov01@gmail.com>
2022-03-23 17:47:04 +0500

    add

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-// let elMenuBtn = document.getElementById('menuBtn');
-// let elMenuBox = document.getElementById('menuBox');
-// let elMenuRemov = document.getElementById('menuRemov')
+const elInput1 = document.querySelector('.input1');
+const elInput2 = document.querySelector('.input2');
+const elInput3 = document.querySelector('.input3');
+const elRenge1 = document.querySelector('.ran1');
+const elRenge2 = document.querySelector('.ran2');
+const elRenge3 = document.querySelector('.ran3');
 
-// elMenuBtn.addEventListener('click', function () {
-//   elMenuBox.classList.add('open-menu');
-// })
+elInput1.addEventListener('keyup', funRange);
+elInput2.addEventListener('keyup', funRange);
+elInput3.addEventListener('keyup', funRange);
 
-// elMenuRemov.addEventListener('click', function () {
-//   elMenuBox.classList.remove('open-menu');
-// })
+function funRange() {
+  let n = 100 / elInput1.getAttribute("maxlength");
+  elRenge1.style.width = `${elInput1.value.length * n}%`;
+  elRenge2.style.width = `${elInput2.value.length}%`;
+  elRenge3.style.width = `${elInput3.value.length / 10}%`;
+}
